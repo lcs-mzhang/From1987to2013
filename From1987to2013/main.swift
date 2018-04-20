23a18f2b823e2670ccfe7e50e0187365c5db65b5
Zhang Matt <mzhang@lcs.on.ca>
2018-04-20 14:22:11 -0400

    COMPLETED

--- From1987to2013/main.swift
+++ From1987to2013/main.swift
@@ -8,10 +8,42 @@
 
 import Foundation
 
-// Get the user input
-var rawInput = readLine()
+var givenInput : String
+var inputYear = 0
 
-// Print out the input provided
-print("You said:")
-print(rawInput)
+var nextDistinct : Int
+var testingYear : String
+
+while true {
+    guard let givenInput = readLine() else {
+        continue
+    }
+    guard let integerInput = Int(givenInput) else {
+        continue
+    }
+    inputYear = integerInput
+    break
+}
+
+while true {
+    inputYear += 1
+    testingYear = String(inputYear)
+    
+    var frequency : [Character : Int] = [:]
+    for character in testingYear {
+        if frequency[character] == nil {
+            frequency[character] = 1
+        } else {
+            frequency[character]! += 1
+        }
+    }
+    
+    if frequency.count == 4 {
+        print("The next distinct year is \(testingYear)")
+        break
+    } else {
+        //REMOVE ENTRIES IN DICTIONARY HERE
+        continue
+    }
+}
 
